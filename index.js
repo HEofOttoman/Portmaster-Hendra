2c54c2a201fbb30add61919b830165b5e8cd7f7d
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-06-11 13:21:03 +1000

    Whar?

---
+++ index.js
@@ -0,0 +1,24 @@
+// require("dotenv").config();
+
+// const { App } = require("@slack/bolt");
+import { App } from "npm:@slack/bolt";
+
+// import "jsr:@std/dotenv/load";
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true
+});
+
+app.command("/hendra", async({ command, ack, respond }) => {
+    const start = Date.now();
+    await ack();
+    const latency = Date.now() - start;
+    await respond({ text: `Pong!\nLatency: ${latency}ms` })
+});
+
+(async () => {
+    await app.start();
+    console.log("Hendra is awake!")
+})();

c1f1349271e281d97204f37edb4ed96ac2370009
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-06-15 22:28:23 +1000

    added joiner and removed index.js
    ran deno fmt too

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Portmaster Hendra
-An assistant & bot for my personal Slack channel, made using the Slack BoltJS SDK. I use Deno because why not.
+
+An assistant & bot for my personal Slack channel, made using the Slack BoltJS
+SDK. I use Deno because why not.
 
 ## Contributors
+
 - Henry Wauzivuff
 - sonion it's me
 
--- index.ts
+++ index.ts
@@ -6,77 +6,79 @@ import { App } from "@slack/bolt";
 
 // import "jsr:@std/dotenv/load";
 import "@std/dotenv/load";
+// import { ClientRequest } from "node:http";
 
-// import PROMPT from "prompt.md";
+// import PROMPT from "pr
+type TriggerType = "ping"; // gorkie
+
+interface Trigger {
+  type: TriggerType;
+  info: string | string[];
+}
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    appToken: process.env.SLACK_APP_TOKEN,
-    socketMode: true
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
 });
 
-app.command("/hendra", async({ command, ack, respond }) => {
-    await ack();
-    await respond({ text: `yes what whar huh what?` })
+app.command("/hendra", async ({ command, ack, respond }) => {
+  await ack();
+  await respond({ text: `yes what whar huh what?` });
 });
 
-app.command("/hendra-ping", async({ command, ack, respond }) => {
-    const start = Date.now();
-    await ack();
-    const latency = Date.now() - start;
-    await respond({ text: `ugh fine. Pong!\nLatency: ${latency}ms` })
+app.command("/hendra-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `ugh fine. Pong!\nLatency: ${latency}ms` });
 });
 
-app.command("/hendra-help", async({ command, ack, respond }) => {
-    await ack();
-    await respond({ 
-        text:
-        `Commands:
+app.command("/weather-ports", async ({ command, ack, respond }) => {
+  await ack();
+  await respond({
+    text: "",
+  });
+});
+
+app.command("/hendra-help", async ({ command, ack, respond }) => {
+  await ack();
+  await respond({
+    text: `Commands:
         /hendra-ping - speed
         /hendra - huh
-        /hendra-help`
-    })
+        /hendra-help`,
+  });
 });
 
-app.event('reaction_added', async ({ event, say, client, logger }) => {
-    try {
-
-        const channelId = event.item.channel;
-        const threadTs = event.item.ts;
-        
-        const message = await client.conversations.history({
-            channel: channelId,
-            latest: threadTs,
-            inclusive: true,
-            limit: 1, // We only want the message that was reacted to
-        });
-
-        const messageText = message.messages[0].text;
-
-        const messages = [{role: 'user', content: messageText}];
-        
-        
-        // Post a confirmation message in the thread
-        const initialMessage = await say({
-            text: `Hello, I'm a Code Assistant app working on your behalf! I'm asking AI your question: ${messageText}`,
-            channel: channelId,
-            thread_ts: threadTs, // This starts the thread if one doesn't exist
-        });
-
-        const initialMessageTs = initialMessage.ts;
-        
-        await say({
-            text: 'ayo what',
-            channel: channelId,
-            thread_ts: initialMessageTs,
-        });
-    } catch (error) {
-        logger.error('Error handling event:', error)
-    }
-
+app.event("member_joined_channel", async ({ event, say, client, logger }) => {
+  try {
+    await client.chat.postMessage({
+      channel: event.channel,
+      text:
+        `<@${event.user}> :wavey: Welcome to Henry's channel! <@${`U080F22CTJN`}>, greet your guest. Behave, or I might have to █████ ███ ██████ :>.`,
+    });
+  } catch (error) {
+    logger.error("Error handling event:", error);
+    console.log(error);
+  }
 });
+
+app.event("member_left_channel", async ({ event, say, client, logger }) => {
+  try {
+    await client.chat.postMessage({
+      channel: event.channel,
+      text: `bye...`,
+    });
+  } catch (error) {
+    logger.error("Error handling event:", error);
+    console.log("Error handling event:", error);
+  }
+});
+
+// app.event("app_mention", async ({ event, say, client, logger}) => {})
 
 (async () => {
-    await app.start();
-    console.log("Hendra is awake!")
+  await app.start();
+  console.log("Hendra is awake!");
 })();
